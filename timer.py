f1012701bb8879bea3c522b45e5145606a0375d3
randombauer <randombaur@gmail.com>
2014-09-09 23:47:02 +0200

    count seconds
    first try at counting turns

--- timer.py
+++ timer.py
@@ -2,4 +2,20 @@ def get_pixel_colour(i_x, i_y):
 	import PIL.ImageGrab
 	return PIL.ImageGrab.grab().load()[i_x, i_y]
  
-print(get_pixel_colour(0, 0))	
+def check_colour():
+	colour = check_pixel_colour(x, y)
+	if colour == yellow || colour == green
+		return 1
+	else:
+		return 0
+
+def count()
+	while(get_pixel_colour(x_game, y_game)
+		if (check_colour(x, y) == 1)
+			my_counter += $OneSecond
+		else:
+			their_counter += 1
+		overall_counter += 1
+
+		
+	
