21ccdd3b59fdbc9095f5a2d127cb6ffa411e32e0
Matthias Grießmeier <mgriessmeier@suse.de>
2014-09-15 10:23:18 +0200

    some more test functionality

--- colour.py
+++ colour.py
@@ -17,9 +17,27 @@ class Colour:
         self.green = (63, 131, 6)
 
     def get_pixel_colour(self):
-        buff = gtk.gdk.Pixbuf(gtk.gdk.COLORSPACE_RGB, False, 8, 1, 1)
-        buff.get_from_drawable(gtk.gdk.get_default_root_window(), gtk.gdk.colormap_get_system(), self.x, self.y, 0, 0, 1, 1)
-        return tuple(buff.get_pixels_array().tolist()[0][0])
+        if sys.argv[1] == "test":
+            print "=== Entering Testmode ==="
+            for img in os.listdir('img/'):
+                im = Image.open("img/"+img)
+                pix = im.convert("RGB")
+                r,g,b = pix.getpixel((1046, 336))
+
+                if 35 < r < 180 and 150 < g < 255 and 0 < b < 175:
+                    print ("%s: (%s, %s, %s) ==> Green" % (img, r, g, b))
+                elif 100 < r < 255 and 120 < g < 255 and 0 < b < 60:
+                    print ("%s: (%s, %s, %s) ==> Yellow" % (img, r, g, b))
+                else:
+                    print ("%s: (%s, %s, %s) ==> Grey or different Color" % (img, r, g, b))
+
+            print "=== Test finished ==="
+            sys.exit(0)
+
+        else:
+            buff = gtk.gdk.Pixbuf(gtk.gdk.COLORSPACE_RGB, False, 8, 1, 1)
+            buff.get_from_drawable(gtk.gdk.get_default_root_window(), gtk.gdk.colormap_get_system(), self.x, self.y, 0, 0, 1, 1)
+            return tuple(buff.get_pixels_array().tolist()[0][0])
 
     def check_colour(self):
         colour = self.get_pixel_colour()
@@ -28,34 +46,11 @@ class Colour:
         else:
             return 0
 
-    def check_colour_test(self):
-        colour = self.get_pixel_colour_test()
-        if colour == self.yellow or colour == self.green:
-            return 1
-        else:
-            return 0
-
-    def get_pixel_colour_test(self):
-        for img in os.listdir('img/'):
-            im = Image.open("img/"+img)
-            pix = im.convert("RGB")
-            r,g,b = pix.getpixel((1046, 336))
-            print ("%s: (%s, %s, %s)" % (img, r, g, b))
-        return r,g,b
-
     def test(self):
-
-        if sys.argv[1] == "test":
-            print "== Testmode =="
-            if self.check_colour_test():
-                print "Success"
-            else:
-                print "Eher nicht nein"
+        if self.check_colour():
+            print "Success"
         else:
-            if self.check_colour():
-                print "Success"
-            else:
-                print "Eher nicht nein"
+            print "Eher nicht, nein"
 
     def main(self):
         self.test()
